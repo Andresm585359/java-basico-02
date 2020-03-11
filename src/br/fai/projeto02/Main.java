13ed471090f58ef1517ef4866483e03010da902a
Matuk <mateusmatuk@fai-mg.br>
2020-03-10 22:13:52 -0300

    primeiro up

---
+++ src/br/fai/projeto02/Main.java
@@ -0,0 +1,18 @@
+package br.fai.projeto02;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Main app = new Main();
+		app.Start();
+
+	}
+
+	private void Start() {
+
+		System.out.println("hu3 hu3 br");
+		
+	}
+
+}
